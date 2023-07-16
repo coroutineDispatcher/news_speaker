d49f0e9812521bccf6c027767d7fa7c2402c6997
coroutinedispatcher <stavro96@gmail.com>
2023-07-16 21:19:39 +0200

    fix error on text input fragment

--- app/src/main/java/com/coroutinedispatcher/newsspeaker/ui/textinput/TextInputFragment.kt
+++ app/src/main/java/com/coroutinedispatcher/newsspeaker/ui/textinput/TextInputFragment.kt
@@ -1,13 +1,33 @@
 package com.coroutinedispatcher.newsspeaker.ui.textinput
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.fragment.app.Fragment
+import com.coroutinedispatcher.newsspeaker.databinding.FragmentTextInputBinding
 
 class TextInputFragment : Fragment() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        error("IllegalStateException")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentTextInputBinding.inflate(inflater, container, false)
+
+        binding.textInputComposable.setContent {
+            Surface() {
+                Text(text = "Hello from Text Input Screen")
+            }
+        }
+        
+        return binding.root
     }
 
     companion object {
